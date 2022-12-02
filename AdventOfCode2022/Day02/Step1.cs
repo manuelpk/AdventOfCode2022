67fda3a4c5dca82bd3ce168601cf1f90d43e5170
Manuel Pokorny <manuel.pokorny@kcc.at>
2022-12-02 07:44:46 +0100

    day 2

---
+++ AdventOfCode2022/Day02/Step1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day02
+{
+    public class Step1
+    {
+        public Step1()
+        {
+            var input = File.ReadAllLines("Day02/Step1Input.txt");
+
+            //rock = 1
+            //paper = 2
+            //scissor = 3
+
+            //lost = 0
+            //draw = 3
+            //win = 6
+
+            //opponent: rock = A, paper = B, scissor = C
+            //me: rock = X, paper = Y, scissor = Z
+
+            var totalPoints = 0;
+
+            foreach (var i in input)
+            {
+                var roundPoints = 0;
+
+                var chosen = i.Split(' ');
+
+                var oppo = GetPoints(chosen[0]);
+                var me = GetPoints(chosen[1]);
+
+                roundPoints = GetMyResult(oppo, me);
+
+                totalPoints += roundPoints;
+            }
+
+            Console.WriteLine($"TotalRoundPoints: {totalPoints}");
+        }
+
+        private int GetPoints(string input)
+        {
+            if (input == "A" || input == "X")
+            {
+                return 1;
+            }
+            else if (input == "B" || input == "Y")
+            {
+                return 2;
+            }
+            else if (input == "C" || input == "Z")
+            {
+                return 3;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private int GetMyResult(int oppo, int me)
+        {
+            if (oppo == me)
+            {
+                return 3 + me;
+            }
+            else if (oppo == 1 && me == 2)
+            {
+                return 6 + me;
+            }
+            else if (oppo == 1 && me == 3)
+            {
+                return 0 + me;
+            }
+            else if (oppo == 2 && me == 1)
+            {
+                return 0 + me;
+            }
+            else if (oppo == 2 && me == 3)
+            {
+                return 6 + me;
+            }
+            else if (oppo == 3 && me == 1)
+            {
+                return 6 + me;
+            }
+            else if (oppo == 3 && me == 2)
+            {
+                return 0 + me;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2022/Day02/Step2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day02
+{
+    public class Step2
+    {
+        public Step2()
+        {
+            var input = File.ReadAllLines("Day02/Step1Input.txt");
+
+            //rock = 1
+            //paper = 2
+            //scissor = 3
+
+            //lost = 0
+            //draw = 3
+            //win = 6
+
+            //opponent: rock = A, paper = B, scissor = C
+            //me: rock = X, paper = Y, scissor = Z
+
+            //X means you need to lose
+            //Y means you need to end the round in a draw
+            //Z means you need to win
+
+            var totalPoints = 0;
+
+            foreach (var i in input)
+            {
+                var roundPoints = 0;
+
+                var chosen = i.Split(' ');
+
+                if (chosen[1] == "X")
+                {
+                    roundPoints = GetLosePoints(GetPoints(chosen[0]));
+                }
+                else if (chosen[1] == "Y")
+                {
+                    roundPoints = GetDrawPoints(GetPoints(chosen[0]));
+                }
+                else if (chosen[1] == "Z")
+                {
+                    roundPoints = GetWinPoints(GetPoints(chosen[0]));
+                }
+
+                totalPoints += roundPoints;
+            }
+
+            Console.WriteLine($"TotalRoundPoints: {totalPoints}");
+        }
+
+        private int GetPoints(string input)
+        {
+            if (input == "A" || input == "X")
+            {
+                return 1;
+            }
+            else if (input == "B" || input == "Y")
+            {
+                return 2;
+            }
+            else if (input == "C" || input == "Z")
+            {
+                return 3;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private int GetLosePoints(int oppo)
+        {
+            if (oppo == 1)
+            {
+                return 3;
+            }
+            else if (oppo == 2)
+            {
+                return 1;
+            }
+            else if (oppo == 3)
+            {
+                return 2;
+            }
+
+            return 0;
+        }
+
+        private int GetDrawPoints(int oppo)
+        {
+            return 3 + oppo;
+        }
+
+        private int GetWinPoints(int oppo)
+        {
+            if (oppo == 1)
+            {
+                return 6 + 2;
+            }
+            else if (oppo == 2)
+            {
+                return 6 + 3;
+            }
+            else if (oppo == 3)
+            {
+                return 6 + 1;
+            }
+
+            return 0;
+        }
+    }
+}
